221c974bb0f18476fb2365c27022b5563567c018
hanseartic <schuette.paul@gmail.com>
2014-08-04 11:26:46 +0200

    Add headers.
    RESTful responses have to use correct headers, depending on the request.

--- application/modules/admin/controllers/TopicsController.php
+++ application/modules/admin/controllers/TopicsController.php
@@ -16,23 +16,26 @@ class Admin_TopicsController extends Zend_Controller_Action
     public function getAction()
     {
         // action body
+        $this->view->message = $this->_getParam('id');
     }
 
     public function postAction()
     {
         // action body
+        $this->getResponse()->setHttpResponseCode(201);
     }
 
     public function putAction()
     {
         // action body
+        $this->getResponse()->setHttpResponseCode(200);
     }
 
     public function deleteAction()
     {
         // action body
+        $this->getResponse()->setHttpResponseCode(204);
     }
-
 
 }
 
